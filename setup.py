94781dba453e5884499e2cc1800e61eae5383dc1
phausamann <peter@hausamann.de>
2019-10-11 16:51:04 -0700

    Fix bug in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='pupil_recording_interface',
     version='0.0.1',
-    packages=[find_packages()],
+    packages=find_packages(),
     long_description=open('README.md').read(),
 )
