b6e0398dfb33a83e8940467f1be14400b6a59826
phausamann <peter@hausamann.de>
2019-10-14 12:47:26 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+requirements = ['numpy', 'pandas', 'xarray', 'scipy', 'msgpack', 'opencv']
+
 setup(
     name='pupil_recording_interface',
     version='0.0.1',
     packages=find_packages(),
     long_description=open('README.md').read(),
+    install_requires=requirements,
 )
