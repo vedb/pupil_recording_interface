4db11051b47ae66b0217b01e635bd7056a75e67f
phausamann <peter.hausamann@tum.de>
2019-11-14 11:26:43 +0100

    Add cli module

---
+++ tests/test_cli.py
@@ -0,0 +1,38 @@
+import os
+
+from .test_base import InterfaceTester
+
+from pupil_recording_interface.cli import CLI
+
+
+class TestCLI(InterfaceTester):
+
+    def test_record(self):
+        """"""
+        # TODO test record odometry
+
+        with self.assertRaises(ValueError):
+            CLI.record(['pri', 'record', 'not_a_topic', '.'])
+
+    def test_export(self):
+        """"""
+        # gaze
+        CLI.export(['pri', 'export', 'gaze', self.folder])
+        assert os.path.exists(
+            os.path.join(self.folder, 'exports', 'gaze.nc'))
+
+        # odometry
+        CLI.export(['pri', 'export', 'odometry', self.folder])
+        assert os.path.exists(
+            os.path.join(self.folder, 'exports', 'odometry.nc'))
+
+        # TODO test export video
+
+        with self.assertRaises(ValueError):
+            CLI.export(['pri', 'export', 'not_a_topic', '.'])
+
+    def test_run(self):
+        """"""
+        CLI().run(['pri', 'export', 'gaze', self.folder])
+        assert os.path.exists(
+            os.path.join(self.folder, 'exports', 'gaze.nc'))
